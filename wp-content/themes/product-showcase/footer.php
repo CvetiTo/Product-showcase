79cca78ebdf46f36b4d94b71ccbfa38052386643
CvetiTo <email@example.com>
2023-05-14 11:02:51 +0300

    Add template-parts

--- wp-content/themes/product-showcase/footer.php
+++ wp-content/themes/product-showcase/footer.php
@@ -1,9 +1,10 @@
 <footer class="site-footer">
-			<p>Copyright <?php echo date('Y') ?> 
-            | <?php echo get_bloginfo( 'title' ); ?>
-			</p>
-		</footer>
-	</div>
-    <?php wp_footer(); ?>
+	<p>Copyright <?php echo date('Y') ?>
+		| <?php echo get_bloginfo('title'); ?>
+	</p>
+</footer>
+</div>
+<?php wp_footer(); ?>
 </body>
+
 </html>
--- wp-content/themes/product-showcase/header.php
+++ wp-content/themes/product-showcase/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="profile" href="//gmpg.org/xfn/11" />
@@ -13,8 +14,17 @@
 
 	<?php wp_head(); ?>
 </head>
+
 <body>
 	<div class="site-wrapper">
 		<header class="site-header">
-			<h1 class="site-title"><a href="#">Online Shop</a></h1>
+			<?php if (is_home()) : ?>
+				<h1 class="site-title">
+					<a href="<?php echo esc_url(get_home_url()); ?>">Online Shop</a>
+				</h1>
+			<?php else : ?>
+				<p class="site-title">
+					<a href="<?php echo esc_url(get_home_url()); ?>">Online Shop</a>
+				</p>
+			<?php endif; ?>
 		</header>
