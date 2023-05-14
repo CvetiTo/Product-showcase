a3a77102d99e9714b40ae3e66136cf02e58507c3
CvetiTo <email@example.com>
2023-05-14 10:22:19 +0300

    Enqueue styles

---
+++ wp-content/themes/product-showcase/functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This function takes care the assets with enqueue
+ *
+ * @return void
+ */
+function product_assets() {
+    wp_enqueue_style( 'product-showcase',
+     get_stylesheet_directory_uri() . '/assets/css/master.css', 
+     array(),
+    filemtime( get_template_directory() . '/assets/css/master.css') );
+}
+add_action( 'wp_enqueue_scripts', 'product_assets' );
