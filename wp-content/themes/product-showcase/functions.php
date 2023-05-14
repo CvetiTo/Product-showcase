026600f2d7702d0d65f46d93be2ea11e1dad8ae4
CvetiTo <email@example.com>
2023-05-14 15:56:54 +0300

    Registered  WordPress menu

--- wp-content/themes/product-showcase/functions.php
+++ wp-content/themes/product-showcase/functions.php
@@ -24,7 +24,6 @@ add_action('wp_enqueue_scripts', 'product_assets');
  */
 function detect_word($content)
 {
-    //var_dump( $content );
     $search = 'Welcome';
     if (str_contains($content, $search)) {
         $new_str = str_replace($search, '<span style="color:green">NEW-TEST-WORD</span>', $content);
@@ -34,3 +33,15 @@ function detect_word($content)
     return $new_str;
 }
 add_filter('the_content', 'detect_word');
+
+/**
+  * Taking care of our custom menu
+  *
+  * @return void
+  */
+  function product_register_nav_menu(){
+    register_nav_menus( array(
+        'primary_menu' => __( 'Primary Menu', 'product' )
+    ) );
+  }
+  add_action( 'after_setup_theme', 'product_register_nav_menu', 0 );
--- wp-content/themes/product-showcase/header.php
+++ wp-content/themes/product-showcase/header.php
@@ -28,3 +28,12 @@
 				</p>
 			<?php endif; ?>
 		</header>
+		<div class="header-nav-menu">
+			<?php
+			if (has_nav_menu('primary_menu')) {
+				wp_nav_menu(array(
+					'theme_location' => 'primary_menu',
+				));
+			}
+			?>
+		</div>
