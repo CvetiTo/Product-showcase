df98d1ca858f679017f177bfb935dcecf3d2ecc8
CvetiTo <email@example.com>
2023-05-14 10:04:54 +0300

    Add header and footer files

---
+++ wp-content/themes/product-showcase/footer.php
@@ -0,0 +1,9 @@
+<footer class="site-footer">
+			<p>Copyright <?php echo date('Y') ?> 
+            | <?php echo get_bloginfo( 'title' ); ?>
+			</p>
+		</footer>
+	</div>
+    <?php wp_footer(); ?>
+</body>
+</html>
