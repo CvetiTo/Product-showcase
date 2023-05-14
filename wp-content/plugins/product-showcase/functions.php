a8dc8b0bca71b529c3912a33b3223114311980ee
CvetiTo <email@example.com>
2023-05-14 14:43:30 +0300

    Add shortcode -display current user name and URL

--- wp-content/plugins/product-showcase/functions.php
+++ wp-content/plugins/product-showcase/functions.php
@@ -8,12 +8,6 @@
  */
 function product_update_items_visit_count($post_id = 0)
 {
-    //if ( is_singular( 'item' ) ) {
-    //    var_dump('yes, we are in the items page!'); die();
-    //} else {
-    //    var_dump('in else');
-    //}
-    //die();
     if (empty($post_id)) {
         return;
     }
@@ -27,3 +21,26 @@ function product_update_items_visit_count($post_id = 0)
         update_post_meta($post_id, 'visits_counts', '1');
     }
 }
+
+/**
+ * Display the current user name if the user is logged in
+ *
+ * @return void
+ */
+function product_showcase_display_username()
+{
+    $output = '';
+    if (is_user_logged_in()) {
+        //var_dump('Yes, we are logged in!');
+        $current_user = wp_get_current_user();
+        //var_dump($current_user);
+        $user_display_name = $current_user->data->display_name;
+        $user_url = $current_user->data->user_url;
+        $output = 'Hey ' . strtoupper($user_display_name) . ', you are the BEST!!! ' . '<br>' .
+            'My URL is: ' . $user_url;
+    } else {
+        $output = 'No, you are not logged in!';
+    }
+    return $output;
+}
+add_shortcode('display_username', 'product_showcase_display_username');
